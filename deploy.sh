421f070d43e901d54a7dae40883ed4dc4b996479
Shoota Kumano <shoota.dev@gmail.com>
2014-08-16 18:05:54 +0900

    auto commit from deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,13 @@
 
 echo "deploy start\n"
 
+## for builder
+git checkout builder
+git add -A
+git commit -m "auto commit from deploy.sh"
+git push origin builder
+
+## for master
 wintersmith build --clean -o build
 cp -a build ../twilight-build-temp
 git checkout master
@@ -10,3 +17,8 @@ git checkout master
 cp -a ../twilight-build-temp/* ./
 rm -rf ../twilight-build-temp
 
+git add -A
+git commit -m "auto commit from deploy.sh"
+git push origin master
+
+echo "\n\ndone!"
